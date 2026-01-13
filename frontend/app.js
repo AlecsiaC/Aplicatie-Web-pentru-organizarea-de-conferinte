84bbabe87b08280db793b98f942287ce005d7dfd
deliamaria23 <deliamariadorobantu@gmail.com>
2026-01-13 21:49:03 +0200

    Am adaugat functionalitate pt deploy

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,6 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
+    ? 'http://localhost:3000/api' 
+    : 'https://aplicatie-web-pentru-organizarea-de.onrender.com/api';
 let currentConferenceId = null; // NOU: Salvăm ID-ul conferinței curente
 
 // 1. Funcție pentru Înregistrare (POST /api/utilizatori)
